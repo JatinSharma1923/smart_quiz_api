bcdcf82f1e8ebfc88cb48e7bd9f01dc254d509d2
JATIN SHARMA <jpsharma1923@gmail.com>
2025-07-03 06:39:20 +0530

    Update main

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+
+
 from fastapi import FastAPI
 from routers import quiz, user
 
 app = FastAPI()
-app.include_router(quiz.router)
-app.include_router(user.router)
+
+app.include_router(quiz.router, prefix="/quiz", tags=["Quiz"])
+app.include_router(user.router, prefix="/user", tags=["User"])
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to Smart Quiz API"}
+

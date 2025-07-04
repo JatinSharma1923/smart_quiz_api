397a2175f24f9aa5a8024f2cc1b44ff002abcba8
JATIN SHARMA <jpsharma1923@gmail.com>
2025-07-04 16:14:09 +0530

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,2 +1,6 @@
-# Empty or with:
-# from .quiz import generate
+from .quiz_router import router as quiz_router
+from .user_router import router as user_router
+from .admin_router import router as admin_router
+app.include_router(quiz_router, prefix="/quiz")
+app.include_router(user_router, prefix="/user")
+app.include_router(admin_router, prefix="/admin")
